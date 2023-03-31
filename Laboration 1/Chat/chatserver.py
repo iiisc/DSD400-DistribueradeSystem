cb95af1a599a4d8c60dbb62a758c531354b66580
iiisc <63256807+iiisc@users.noreply.github.com>
2023-03-31 15:52:01 +0200

    Changes

--- Laboration 1/Chat/chatserver.py
+++ Laboration 1/Chat/chatserver.py
@@ -9,8 +9,7 @@ PORT = 1234
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
 
-with SimpleXMLRPCServer((HOST[0], PORT),
-                        requestHandler=RequestHandler, allow_none=True, logRequests=False) as server:
+with SimpleXMLRPCServer((HOST[0], PORT), requestHandler=RequestHandler, allow_none=True, logRequests=False) as server:
     server.register_introspection_functions()
 
     def broadcast(msg):
