209d319187f261cc085d5a4df932d96c56da7e7a
iiisc <63256807+iiisc@users.noreply.github.com>
2023-04-11 21:17:25 +0200

    Changes

--- Hemsida/js.js
+++ Hemsida/js.js
@@ -1,4 +1,9 @@
-function smc(){
-    document.getElementById('b1').classList.replace("danger", "safe");
-    document.getElementById('h1').innerHTML = "SAFE";
+function toggleTheme(){
+    if(document.getElementById('b1').className == "danger") {
+        document.getElementById('b1').classList.replace("danger", "safe");
+        document.getElementById('h1').innerHTML = "SAFE";
+    } else {
+        document.getElementById('b1').classList.replace("safe", "danger");
+        document.getElementById('h1').innerHTML = "DANGER";
+    }
 }
