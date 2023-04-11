d8c166f7b9b893487d844494c3d423c826280500
iiisc <63256807+iiisc@users.noreply.github.com>
2023-04-11 21:12:44 +0200

    many changes

---
+++ Hemsida/js.js
@@ -0,0 +1,4 @@
+function smc(){
+    document.getElementById('b1').classList.replace("danger", "safe");
+    document.getElementById('h1').innerHTML = "SAFE";
+}
--- Hemsida/runWebsite.py
+++ Hemsida/runWebsite.py
@@ -2,14 +2,10 @@ import http.server
 import socketserver
 
 PORT = 80
-PATH = "/index.html" 
-#PATH = "C:/Users/Fractal ERA/Desktop/WEBSITE/index.html"
-#PATH = "C:\Users\Fractal ERA\Desktop\WEBSITE\index.html"
-#PATH = "C:\\Users\\Fractal ERA\\Desktop\\WEBSITE\\index.html"
+
 
 class MyRequestHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
-        self.path = PATH
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
 Handler = MyRequestHandler
